2553958839962b2b2ea8ba3f060f49a8c014bc59
Brandon Lim Khin Wai <brandon.lim0499@gmail.com>
2022-03-06 01:51:57 +0800

    footer completed

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,17 @@
     <MarketingSection></MarketingSection>
     <CompanyInfoSection></CompanyInfoSection>
     <ParticipateSection></ParticipateSection>
-    <Footer :companyLogo="companyLogo"></Footer>
+    <Footer
+      :companyLogo="footer.companyLogo"
+      :companyDescription="footer.companyDescription"
+      :footerLists="footer.footerLists"
+      :socialMediaLinks="footer.socialMediaLinks"
+      :companyName="footer.companyName"
+      :creatorCompany="footer.creatorCompany"
+      :creatorCompanyWebsite="footer.creatorCompanyWebsite"
+      :creatorName="footer.creatorName"
+      :creatorLink="footer.creatorLink"
+    ></Footer>
   </div>
 </template>
 
@@ -35,7 +45,158 @@ import Footer from "./components/Footer.vue";
 import { ref } from "@vue/reactivity";
 
 // Footer data
-const companyLogo = ref("/assets/61f314a5cce72f5bd933270d_Friday-black.svg");
+// const companyLogo = ref("/assets/61f314a5cce72f5bd933270d_Friday-black.svg");
+const footer = ref({
+  companyLogo: "/assets/61f314a5cce72f5bd933270d_Friday-black.svg",
+  companyDescription:
+    "Friday Festival is a modern Webflow Template for conference & event websites",
+  footerLists: [
+    {
+      footerListName: "Landings",
+      footerSubLists: [
+        {
+          footerSubListName: "Landing 01",
+          sublistLink: "https://friday-template.webflow.io/landing/01",
+        },
+        {
+          footerSubListName: "Landing 02",
+          sublistLink: "https://friday-template.webflow.io/landing/02",
+        },
+        {
+          footerSubListName: "Landing 03",
+          sublistLink: "https://friday-template.webflow.io/landing/03",
+        },
+        {
+          footerSubListName: "Landing 04",
+          sublistLink: "https://friday-template.webflow.io/landing/04",
+        },
+        {
+          footerSubListName: "Landing 05",
+          sublistLink: "https://friday-template.webflow.io/landing/05",
+        },
+      ],
+    },
+    {
+      footerListName: "Features",
+      footerSubLists: [
+        {
+          footerSubListName: "Features 01",
+          sublistLink: "https://friday-template.webflow.io/features/01",
+        },
+        {
+          footerSubListName: "Features 02",
+          sublistLink: "https://friday-template.webflow.io/features/02",
+        },
+        {
+          footerSubListName: "Features 03",
+          sublistLink: "https://friday-template.webflow.io/features/03",
+        },
+        {
+          footerSubListName: "Features 04",
+          sublistLink: "https://friday-template.webflow.io/features/04",
+        },
+        {
+          footerSubListName: "Features 05",
+          sublistLink: "https://friday-template.webflow.io/features/05",
+        },
+      ],
+    },
+    {
+      footerListName: "CMS",
+      footerSubLists: [
+        {
+          footerSubListName: "CMS 01",
+          sublistLink: "https://friday-template.webflow.io/cms/cms-01",
+        },
+        {
+          footerSubListName: "CMS 02",
+          sublistLink: "https://friday-template.webflow.io/cms/cms-02",
+        },
+        {
+          footerSubListName: "CMS 03",
+          sublistLink: "https://friday-template.webflow.io/cms/cms-03",
+        },
+        {
+          footerSubListName: "CMS 04",
+          sublistLink: "https://friday-template.webflow.io/cms/cms-04",
+        },
+        {
+          footerSubListName: "CMS 05",
+          sublistLink: "https://friday-template.webflow.io/cms/cms-05",
+        },
+      ],
+    },
+    {
+      footerListName: "Form",
+      footerSubLists: [
+        {
+          footerSubListName: "Form 01",
+          sublistLink: "https://friday-template.webflow.io/other-pages/form-01",
+        },
+        {
+          footerSubListName: "Form 02",
+          sublistLink: "https://friday-template.webflow.io/other-pages/form-02",
+        },
+        {
+          footerSubListName: "Form 03",
+          sublistLink: "https://friday-template.webflow.io/other-pages/form-03",
+        },
+        {
+          footerSubListName: "Form 04",
+          sublistLink: "https://friday-template.webflow.io/other-pages/form-04",
+        },
+        {
+          footerSubListName: "Form 05",
+          sublistLink: "https://friday-template.webflow.io/other-pages/form-05",
+        },
+      ],
+    },
+    {
+      footerListName: "Template",
+      footerSubLists: [
+        {
+          footerSubListName: "Styleguide",
+          sublistLink: "https://friday-template.webflow.io/utility/style-guide",
+        },
+        {
+          footerSubListName: "License",
+          sublistLink: "https://friday-template.webflow.io/utility/licenses",
+        },
+        {
+          footerSubListName: "Changelog",
+          sublistLink: "https://friday-template.webflow.io/utility/change-log",
+        },
+      ],
+    },
+  ],
+  socialMediaLinks: [
+    {
+      link: "https://friday-template.webflow.io/landing/01#",
+      image: "/assets/61e5bb75ba3a7edc0f0bd4a4_icon-facebook.svg",
+    },
+    {
+      link: "https://friday-template.webflow.io/landing/01#",
+      image: "/assets/61e5bb75ba3a7e5ded0bd4a5_icon-instagram.svg",
+    },
+    {
+      link: "https://friday-template.webflow.io/landing/01#",
+      image: "/assets/61e5bb75ba3a7ee67e0bd4a2_icon-twitter.svg",
+    },
+    {
+      link: "https://friday-template.webflow.io/landing/01#",
+      image: "/assets/61e5bb75ba3a7e5fe50bd4a6_icon-linkedin.svg",
+    },
+    {
+      link: "https://friday-template.webflow.io/landing/01#",
+      image: "/assets/61e5bb75ba3a7ee2f50bd4a7_icon-youtube.svg",
+    },
+  ],
+  companyName: "Friday",
+  creatorCompany: "THC",
+  creatorCompanyWebsite: "http://www.webflow.com/",
+  creatorName: "JP",
+  creatorLink: "https://webflow.com/templates/designers/jp",
+});
 </script>
 
 <style></style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,248 +8,39 @@
         >
           <img :src="companyLogo" loading="lazy" alt="" class="logo-footer" />
           <p class="main-paragraph">
-            Friday Festival is a modern Webflow Template for conference &amp;
-            events websites.
+            {{ companyDescription }}
           </p>
         </div>
         <ul
-          id="w-node-_8241a9fd-f946-6e5c-77d3-1bc5fca6053d-07bf8e43"
           role="list"
           class="w-list-unstyled"
+          v-for="list in footerLists"
+          :key="list"
         >
-          <li><h4 class="heading-small text-gray">Landings</h4></li>
           <li>
-            <a
-              href="https://friday-template.webflow.io/landing/01"
-              aria-current="page"
-              class="link-footer w--current"
-              >Landing 01</a
-            >
+            <h4 class="heading-small text-gray">
+              {{ list.footerListName }}
+            </h4>
           </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/landing/02"
-              class="link-footer"
-              >Landing 02</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/landing/03"
-              class="link-footer"
-              >Landing 03</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/landing/04"
-              class="link-footer"
-              >Landing 04</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/landing/05"
-              class="link-footer"
-              >Landing 05</a
-            >
+
+          <li v-for="sublist in list.footerSubLists" :key="sublist">
+            <a :href="sublist.sublistLink" class="link-footer">{{
+              sublist.footerSubListName
+            }}</a>
           </li>
         </ul>
-        <ul
-          id="w-node-_09fbdce1-9e10-0457-7f74-d3544bfc5db7-07bf8e43"
-          role="list"
-          class="w-list-unstyled"
-        >
-          <li><h4 class="heading-small text-gray">Features</h4></li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/features/01"
-              class="link-footer"
-              >Features 01</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/features/02"
-              class="link-footer"
-              >Features 02</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/features/03"
-              class="link-footer"
-              >Features 03</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/features/04"
-              class="link-footer"
-              >Features 04</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/features/05"
-              class="link-footer"
-              >Features 05</a
-            >
-          </li>
-        </ul>
-        <ul
-          id="w-node-_6b00483b-9d6f-1b79-81ab-0281770edf52-07bf8e43"
-          role="list"
-          class="w-list-unstyled"
-        >
-          <li><h4 class="heading-small text-gray">CMS</h4></li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/cms/cms-01"
-              class="link-footer"
-              >CMS 01</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/cms/cms-02"
-              class="link-footer"
-              >CMS 02</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/cms/cms-03"
-              class="link-footer"
-              >CMS 03</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/cms/cms-04"
-              class="link-footer"
-              >CMS 04</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/cms/cms-05"
-              class="link-footer"
-              >CMS 05</a
-            >
-          </li>
-        </ul>
-        <ul
-          id="w-node-_476684f0-a868-66b7-7616-6ad8bc4dea3a-07bf8e43"
-          role="list"
-          class="w-list-unstyled"
-        >
-          <li><h4 class="heading-small text-gray">Form</h4></li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/other-pages/form-01"
-              class="link-footer"
-              >Form 01</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/other-pages/form-02"
-              class="link-footer"
-              >Form 02</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/other-pages/form-03"
-              class="link-footer"
-              >Form 03</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/other-pages/form-04"
-              class="link-footer"
-              >Form 04</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/other-pages/form-05"
-              class="link-footer"
-              >Form 05</a
-            >
-          </li>
-        </ul>
-        <ul
-          id="w-node-_2fe9a4b0-64a0-cd1d-1f0f-542cb6b628dc-07bf8e43"
-          role="list"
-          class="w-list-unstyled"
-        >
-          <li><h4 class="heading-small text-gray">Template</h4></li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/utility/style-guide"
-              class="link-footer"
-              >Styleguide</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/utility/licenses"
-              class="link-footer"
-              >License</a
-            >
-          </li>
-          <li>
-            <a
-              href="https://friday-template.webflow.io/utility/change-log"
-              class="link-footer"
-              >Changelog</a
-            >
-          </li>
-        </ul>
+
         <div
           id="w-node-_0bae9e82-9b29-b0df-f69c-0f24674e559d-07bf8e43"
           class="social-wrapper"
         >
           <a
-            href="https://friday-template.webflow.io/landing/01#"
+            :href="socialMediaLink.link"
             class="link-icons center w-inline-block"
+            v-for="socialMediaLink in socialMediaLinks"
+            :key="socialMediaLink.image"
             ><img
-              src="/assets/61e5bb75ba3a7edc0f0bd4a4_icon-facebook.svg"
-              loading="lazy"
-              alt=""
-              class="icon-social" /></a
-          ><a
-            href="https://friday-template.webflow.io/landing/01#"
-            class="link-icons center w-inline-block"
-            ><img
-              src="/assets/61e5bb75ba3a7e5ded0bd4a5_icon-instagram.svg"
-              loading="lazy"
-              alt=""
-              class="icon-social" /></a
-          ><a
-            href="https://friday-template.webflow.io/landing/01#"
-            class="link-icons center w-inline-block"
-            ><img
-              src="/assets/61e5bb75ba3a7ee67e0bd4a2_icon-twitter.svg"
-              loading="lazy"
-              alt=""
-              class="icon-social" /></a
-          ><a
-            href="https://friday-template.webflow.io/landing/01#"
-            class="link-icons center w-inline-block"
-            ><img
-              src="/assets/61e5bb75ba3a7e5fe50bd4a6_icon-linkedin.svg"
-              loading="lazy"
-              alt=""
-              class="icon-social" /></a
-          ><a
-            href="https://friday-template.webflow.io/landing/01#"
-            class="link-icons center w-inline-block"
-            ><img
-              src="/assets/61e5bb75ba3a7ee2f50bd4a7_icon-youtube.svg"
+              :src="socialMediaLink.image"
               loading="lazy"
               alt=""
               class="icon-social"
@@ -260,8 +51,10 @@
           class="footer-inner"
         >
           <div class="main-paragraph size-small">
-            © Friday — Powered by
-            <a href="http://www.webflow.com/" target="_blank">Webflow</a>
+            © {{ companyName }} — Powered by
+            <a :href="creatorCompanyWebsite" target="_blank">{{
+              creatorCompany
+            }}</a>
           </div>
         </div>
         <div
@@ -270,9 +63,7 @@
         >
           <div class="main-paragraph size-small">
             Created by
-            <a href="https://webflow.com/templates/designers/jp" target="_blank"
-              >JP</a
-            >
+            <a :href="creatorLink" target="_blank">{{ creatorName }}</a>
           </div>
         </div>
       </div>
@@ -286,9 +77,35 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  companyDescription: {
+    type: String,
+    required: true,
+  },
+  footerLists: {
+    type: Array,
+    required: true,
+  },
+  socialMediaLinks: {
+    type: Array,
+  },
+  companyName: {
+    type: String,
+  },
+  creatorCompany: {
+    type: String,
+  },
+  creatorCompanyWebsite: {
+    type: String,
+  },
+  creatorName: {
+    type: String,
+  },
+  creatorLink: {
+    type: String,
+  },
 });
 
-console.log(props);
+// console.log(props.footerLists);
 </script>
 
 <style></style>
